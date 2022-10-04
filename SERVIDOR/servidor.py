1dc90c3837651c50e27018103959752ce8676d53
Santiago Paeres <s.paeres10@uniandes.edu.co>
2022-10-03 19:38:42 -0500

    Servidor 3

--- SERVIDOR/servidor.py
+++ SERVIDOR/servidor.py
@@ -1,12 +1,35 @@
 import socket  # Importa socket
+import hashlib
+
+# Constantes:
+NOMBRE_ARCHIVO_100M = 'archivo_100M'
+NOMBRE_ARCHIVO_250M = 'archivo_250M'
+
+RUTA_ARCHIVO_100M = '../ARCHIVOS/archivo_100M'
+RUTA_ARCHIVO_250M = '../ARCHIVOS/archivo_250M'
+
+
+def hash_archivo(nombre_archivo):
+    md5_hash = hashlib.md5()
+    a_file = ''
+    if nombre_archivo == NOMBRE_ARCHIVO_100M:
+        a_file = open(RUTA_ARCHIVO_100M, "rb")
+    else:
+        a_file = open(RUTA_ARCHIVO_250M, "rb")
+    content = a_file.read()
+    md5_hash.update(content)
+
+    digest = md5_hash.hexdigest()
+    return digest
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Crea Socket TCP
 host = socket.gethostname()  # Toma el nombre de la maquina local
 port = 12345  # Reserva el puerto
 s.bind((host, port))  # Vincula el host al puerto
-f = open('../ARCHIVOS/archivo_100M', 'r')
+
 s.listen(5)  # Espera por la conexion del cliente.
-l = f.read(1024)
+
 while True:
     buffer = ''
     c, addr = s.accept()  # Establece la conexion con el cliente
@@ -17,9 +40,21 @@ while True:
         buffer += data
         print(buffer)
     else:
-        break;
+        break
+    f = ''
+    nombre_archivo = ''
+    if buffer == '100':
+        nombre_archivo = NOMBRE_ARCHIVO_100M
+        f = open(RUTA_ARCHIVO_100M, 'r')
+    else:
+        nombre_archivo = NOMBRE_ARCHIVO_250M
+        f = open(RUTA_ARCHIVO_250M, 'r')
+    hash = hash_archivo(nombre_archivo)
+    l = f.read(1024)
+    print("Enviando Hash...")
+    s.send(hash)
     while l:
-        print("Enviando...")
+        print("Enviando Archivo...")
         s.send(l)
         l = f.read(1024)
     f.close()
