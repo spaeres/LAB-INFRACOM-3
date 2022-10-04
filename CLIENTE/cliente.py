e97be028a01ba85d16c065936d19357f5614e7c7
Juan2707 <47038791+Juan2707@users.noreply.github.com>
2022-10-04 17:08:42 -0500

    esto

--- CLIENTE/cliente.py
+++ CLIENTE/cliente.py
@@ -1,14 +1,15 @@
 import socket
-import sys
 import os
 import errno
 
 #Hacer el archivo hash
-
+nombre = ""
+def nombrar_cliente(n_cliente):
+    nombre = "cliente"+n_cliente
 #Escribir el log(?)
 def escribir_directorio():
     try:
-        os.mkdir('Archivos recibidos')
+        os.mkdir('ArchivosRecibidos')
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
@@ -20,26 +21,30 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port on the server
 # given by the caller
-host = socket.gethostname
+host = socket.gethostname()
 port = 12345
-#server_address = (host, port)
-server_address = (sys.argv[1], 10000)
+server_address = (host, port)
+#server_address = (sys.argv[1], 10000)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
+escribir_directorio()
 
 try:
     #el buffer y todo lo demas
     #necesito archivos?
-    message = b'This is the message.  It will be repeated.'
-    print('sending {!r}'.format(message))
-    sock.sendall(message)
+    
+    #message = b'This is the message.  It will be repeated.'
+    #print('sending {!r}'.format(message))
+    #sock.recv(message)
+    
+    #data1 = sock.recv(1024)
 
-    amount_received = 0
-    amount_expected = len(message)
-    while amount_received < amount_expected:
+    #amount_received = 0
+    #amount_expected = len(message)
+    #while amount_received < amount_expected:
+    while True:
         data = sock.recv(1024)
-        amount_received += len(data)
-        print('received {!r}'.format(data))
+        print('El mensaje fue recibido {!r}'.format(data))
 
 finally:
     sock.close()
