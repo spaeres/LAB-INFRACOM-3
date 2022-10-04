741e9d5be5212c678c6d1c3a6864a27fb7df81c2
Santiago Paeres <s.paeres10@uniandes.edu.co>
2022-10-03 19:10:49 -0500

    Servidor 2

--- SERVIDOR/servidor.py
+++ SERVIDOR/servidor.py
@@ -1,21 +1,29 @@
 import socket  # Importa socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Crea Socket TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Crea Socket TCP
 host = socket.gethostname()  # Toma el nombre de la maquina local
 port = 12345  # Reserva el puerto
 s.bind((host, port))  # Vincula el host al puerto
-f = open('torecv.png', 'wb')
+f = open('../ARCHIVOS/archivo_100M', 'r')
 s.listen(5)  # Espera por la conexion del cliente.
+l = f.read(1024)
 while True:
+    buffer = ''
     c, addr = s.accept()  # Establece la conexion con el cliente
-    print('Got connection from', addr)
-    print("Receiving...")
-    l = c.recv(1024)
+    print('Conexion recivida de: ', addr)
+    # Lee el tipo de archivo que necesita el cliente:
+    data = c.recv(1024)
+    if data:
+        buffer += data
+        print(buffer)
+    else:
+        break;
     while l:
-        print("Receiving...")
-        f.write(l)
-        l = c.recv(1024)
+        print("Enviando...")
+        s.send(l)
+        l = f.read(1024)
     f.close()
-    print("Done Receiving")
-    c.send('Thank you for connecting')
-    c.close()  # Close the connection
+    print("Enviado exitosamente")
+    s.shutdown(socket.SHUT_WR)
+    c.send('Gracias por conectarse. Archivo enviado.')
+    c.close()  # Cerrar conexion
